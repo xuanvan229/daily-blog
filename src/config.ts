2e07c82ca1df28ed50eecb7b6c71eaecfbbc8bdc
xuanvan <xuanvan229@gmail.com>
2024-06-01 22:35:36 +0700

    update

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const SITE_FAVICON = "/favicon.png";
 export const SITE_LOGO = "/avatar.png";
 export const SITE_TITLE = "Daily Notes";
 export const SITE_DESCRIPTION =
-  "Mia is designed to be minimalist yet powerful, integrating the capabilities of Astro, MDX, and Rough Notation. Whether you're a developer, writer, or just someone who loves clean and efficient design, this theme is perfect for you.";
+  "A blog about software development, programming, and life.";
 
 export const MENUS = [
   {
